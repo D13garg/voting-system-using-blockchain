4e5a58b04ec0449be71112d66fcc7759ab9b1c4d
Dron Garg <drongarg13@gmail.com>
2026-07-18 08:59:56 +0530

    update handoff

---
+++ backend/test/types.d.ts
@@ -0,0 +1,11 @@
+declare module "../src/config/env.ts?valid-case" {
+  export * from "../src/config/env.ts";
+}
+
+declare module "../src/config/env.ts?missing-case" {
+  export * from "../src/config/env.ts";
+}
+
+declare module "../src/config/env.ts?malformed-case" {
+  export * from "../src/config/env.ts";
+}
